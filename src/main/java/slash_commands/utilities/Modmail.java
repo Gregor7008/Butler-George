e78036aa925fa6cda29721b70ac5a06ef04dd012
Gregor7008 <Gregor7009@gmail.com>
2022-09-05 22:30:14 +0200

    /modmail open implementation ready
    Note: Not tested yet!

--- src/main/java/slash_commands/utilities/Modmail.java
+++ src/main/java/slash_commands/utilities/Modmail.java
@@ -1,18 +1,30 @@
 package slash_commands.utilities;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import base.Bot;
 import base.assets.AwaitTask;
 import base.engines.LanguageEngine;
 import base.engines.configs.ConfigLoader;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
+import net.dv8tion.jda.api.interactions.callbacks.IModalCallback;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.Modal;
+import net.dv8tion.jda.api.interactions.components.selections.SelectMenu;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import slash_commands.assets.SlashCommandEventHandler;
@@ -41,7 +53,7 @@ public class Modmail implements SlashCommandEventHandler {
 		} else {
 			switch (event.getSubcommandName()) {
 			case "open":
-				this.openOnGuild(event);
+				this.open(event, event.getUser(), event.getGuild());
 				break;
 			case "list":
 				this.list(event);
@@ -68,32 +80,6 @@ public class Modmail implements SlashCommandEventHandler {
 		return command;
 	}
 
-	private void openOnGuild(SlashCommandInteractionEvent event) {
-		User user = event.getUser();
-		Guild guild = event.getGuild();
-		TextInput titleInput = TextInput.create("title", "Title", TextInputStyle.SHORT)
-				.setRequiredRange(5, 200)
-				.setPlaceholder("Input title")
-				.build();
-		TextInput messageInput = TextInput.create("message", "Message", TextInputStyle.PARAGRAPH)
-				.setPlaceholder("Input initial message")
-				.build();
-		Modal modal = Modal.create("modmailModal", "Open Modmail").addActionRows(ActionRow.of(titleInput), ActionRow.of(messageInput)).build();
-		event.replyModal(modal).queue();
-		AwaitTask.forModalInteraction(guild, user, event.getMessageChannel(),
-				mi -> {
-					Category modmailCategory;
-					if (ConfigLoader.INSTANCE.getGuildConfig(guild).getLong("modmailcategory") == 0) {
-						modmailCategory = guild.createCategory("----------📝 ModMail ------------").complete();
-						ConfigLoader.INSTANCE.getGuildConfig(guild).put("modmailcategory", modmailCategory.getIdLong());
-					} else {
-						modmailCategory = guild.getCategoryById(ConfigLoader.INSTANCE.getGuildConfig(guild).getLong("modmailcategory"));
-					}
-					TextChannel ticketChannel = modmailCategory.createTextChannel("").complete();
-					//Continue here
-				}).addValidComponents(modal.getId()).append();
-	}
-
 	private void selectOnGuild(SlashCommandInteractionEvent event) {
 		//List tickets from guild and add select menu, select selected ticket
 	}
@@ -103,7 +89,23 @@ public class Modmail implements SlashCommandEventHandler {
 	}
 
 	private void openOnPrivate(SlashCommandInteractionEvent event) {
-		//Reply with select menu, choose guild, then as in "openOnguild"
+		final User user = event.getUser();
+		SelectMenu.Builder menu = SelectMenu.create("selguild")
+				.setRequiredRange(1, 1)
+				.setPlaceholder("Select server");
+		List<Guild> availableGuilds = Bot.INSTANCE.jda.getGuilds().stream().filter(g -> {
+			try {
+				g.retrieveBan(user).complete();
+				return true;
+			} catch (ErrorResponseException e) {}
+			return g.isMember(user);
+		}).toList();
+		availableGuilds.forEach(g -> menu.addOption(g.getName(), g.getId(), g.getDescription()));
+		Message response = event.replyEmbeds(LanguageEngine.fetchMessage(null, event.getUser(), this, "selguild")).setActionRow(menu.build()).complete().retrieveOriginal().complete();
+		AwaitTask.forSelectMenuInteraction(null, user, response,
+				sm -> {
+					this.open(sm, user, Bot.INSTANCE.jda.getGuildById(sm.getSelectedOptions().get(0).getValue()));
+				});
 	}
 
 	private void selectOnPrivate(SlashCommandInteractionEvent event) {
@@ -116,5 +118,53 @@ public class Modmail implements SlashCommandEventHandler {
 
 	private void list(SlashCommandInteractionEvent event) {
 		//List all tickets by guild and add select menu, reply with button row for selection or closing the ticket
+	}
+	
+	private <T extends IModalCallback> void open(T event, User user, Guild guild) {
+		TextInput titleInput = TextInput.create("title", "Title", TextInputStyle.SHORT)
+				.setRequiredRange(3, 50)
+				.setPlaceholder("Input title")
+				.build();
+		TextInput messageInput = TextInput.create("message", "Message", TextInputStyle.PARAGRAPH)
+				.setPlaceholder("Input initial message")
+				.build();
+		Modal modal = Modal.create("modmailModal", "Open Modmail").addActionRows(ActionRow.of(titleInput), ActionRow.of(messageInput)).build();
+		event.replyModal(modal).queue();
+		AwaitTask.forModalInteraction(guild, user, event.getMessageChannel(),
+				mi -> {
+					JSONObject guildModmail = ConfigLoader.INSTANCE.getGuildConfig(guild).getJSONObject("modmails");
+					JSONObject userModmail = ConfigLoader.INSTANCE.getMemberConfig(guild, user).getJSONObject("modmails");
+					long id = ThreadLocalRandom.current().nextLong(1000,10000);
+					while (guildModmail.keySet().contains(String.valueOf(id)) || userModmail.keySet().contains(String.valueOf(id))) {
+						id = ThreadLocalRandom.current().nextLong(1000,10000);
+					}
+					Category modmailCategory;
+					if (ConfigLoader.INSTANCE.getGuildConfig(guild).getLong("modmailcategory") == 0) {
+						modmailCategory = guild.createCategory("----------📝 ModMail ------------").complete();
+						ConfigLoader.INSTANCE.getGuildConfig(guild).put("modmailcategory", modmailCategory.getIdLong());
+					} else {
+						modmailCategory = guild.getCategoryById(ConfigLoader.INSTANCE.getGuildConfig(guild).getLong("modmailcategory"));
+					}
+					TextChannel ticketChannel = modmailCategory.createTextChannel(user.getName() + "#" + String.valueOf(id)).complete();
+					ticketChannel.upsertPermissionOverride(guild.getPublicRole()).deny(Permission.VIEW_CHANNEL).queue();
+					mi.editMessageEmbeds(LanguageEngine.fetchMessage(guild, user, this, "openSuccess")
+							.replaceDescription("{guild}", guild.getName())
+							.replaceDescription("{mention}", guild.getPublicRole().getAsMention())
+							.replaceDescription("{title}", mi.getValue("title").getAsString())).queue();
+					String addon = "";
+					if (!ConfigLoader.INSTANCE.getGuildConfig(guild).getJSONArray("supportroles").isEmpty()) {
+						ConfigLoader.INSTANCE.getGuildConfig(guild).getJSONArray("supportroles").forEach(sr -> {
+							ticketChannel.upsertPermissionOverride(guild.getRoleById((long) sr)).grant(Permission.VIEW_CHANNEL).queue();
+						});
+					} else {
+						addon = LanguageEngine.getRaw(guild, user, this, "setsupportrole");
+					}
+					Message initialMessage = ticketChannel.sendMessageEmbeds(LanguageEngine.fetchMessage(guild, user, this, "newticket")
+							.replaceDescription("{user}", user.getName())
+							.replaceDescription("{addon}", addon)).complete();
+					guildModmail.put(ticketChannel.getId(), new JSONArray().put(user.getIdLong()).put(id).put(initialMessage.getIdLong()));
+					userModmail.put(String.valueOf(id), new JSONArray().put(guild.getIdLong()).put(ticketChannel.getIdLong()));
+					ticketChannel.sendMessage("**" + mi.getValue("title").getAsString() + "**\n\n" + mi.getValue("message").getAsString()).queue();
+				}).addValidComponents(modal.getId()).append();
 	}
 }
